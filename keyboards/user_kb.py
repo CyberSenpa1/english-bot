951fabd212db1c0672a5e7129e055facd768a68f
CyberSenpa1 <valeriysimachenkov@gmail.com>
2025-06-29 21:56:24 +0300

    Клавиатура пользователя

--- keyboards/user_kb.py
+++ keyboards/user_kb.py
@@ -5,14 +5,15 @@ start_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Изучение английского')],
     [KeyboardButton(text='Статистика')],
     [KeyboardButton(text='О нас')]
-]
+], resize_keyboard=True, one_time_keyboard=True
 )
 
 learn_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Старт")],
     [KeyboardButton(text="Сложность")],
     [KeyboardButton(text="Назад")],
-])
+], resize_keyboard=True, one_time_keyboard=True
+)
 
 difficult = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="1")],
@@ -20,7 +21,8 @@ difficult = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="3")],
     [KeyboardButton(text="4")],
     [KeyboardButton(text="5")]
-])
+], resize_keyboard=True, one_time_keyboard=True
+)
 
 statistic_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Назад")]
